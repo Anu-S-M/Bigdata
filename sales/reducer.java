53d6666154a33c90120d02e3c7f32028fb3017b0
Anu S M <117372780+Anu-S-M@users.noreply.github.com>
2024-05-19 23:41:15 +0530

    Create reducer.java

---
+++ sales/reducer.java
@@ -0,0 +1,18 @@
+package sales;
+import java.io.*;
+import java.util.*;
+import org.apache.hadoop.mapred.*;
+import org.apache.hadoop.io.*;
+
+public class reducer extends MapReduceBase implements Reducer<Text,IntWritable,Text,IntWritable>
+{
+public void reduce(Text key,Iterator <IntWritable> value,OutputCollector<Text,IntWritable> output ,Reporter r) throws IOException
+{
+int sum=0;
+while(value.hasNext())
+{
+sum+=value.next().get();
+}
+output.collect(new Text(key),new IntWritable(sum));
+}
+}
