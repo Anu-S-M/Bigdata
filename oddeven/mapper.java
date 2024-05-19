44e00b4ff9aad8947f2b6ca08f38b09a1bc1f22f
Anu S M <117372780+Anu-S-M@users.noreply.github.com>
2024-05-19 23:25:38 +0530

    Create mapper.java

---
+++ oddeven/mapper.java
@@ -0,0 +1,21 @@
+package oddeven2;
+import java.io.*;
+import java.util.*;
+import org.apache.hadoop.mapred.*;
+import org.apache.hadoop.io.*;
+public class mapper extends MapReduceBase implements Mapper<LongWritable , Text , Text ,IntWritable>
+{
+public void map(LongWritable key,Text value,OutputCollector<Text,IntWritable> output,Reporter r) throws IOException
+{
+String[] line=value.toString().split(" ");
+for(String num:line){
+int number=Integer.parseInt(num);
+if(number%2==0) {
+output.collect(new Text("even"),new IntWritable(number));
+}
+else{
+output.collect(new Text("odd"),new IntWritable(number));
+}
+}
+}
+}
